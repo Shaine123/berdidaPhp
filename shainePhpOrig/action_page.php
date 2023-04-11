2ea8c40d85b8db4ff51662ecafbf4bebe161d210
Shaine123 <shaineberdida@gmail.com>
2023-04-11 03:47:18 -0700

    add new action page

---
+++ shainePhpOrig/action_page.php
@@ -0,0 +1,21 @@
+<?php require 'includes/header.php'?>
+ <h1 style="color:green; text-align: center;">YOU HAVE BEEN REGISTERED</h1>
+<?php
+   $firstname = $_REQUEST['fname'];
+   $lastname = $_REQUEST['lname'];
+   $date = $_REQUEST['date'];
+   $specialty = $_REQUEST['specialty'];
+   $phone = $_REQUEST['phone'];
+//    $check = $_REQUEST['checkbox'];
+ 
+
+   
+?>
+<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Info</h5>
+    <p class="card-text"><?php echo "My name is $firstname $lastname Born on $date . I am $specialty  and you can contact my phone number $phone ";?></p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<?php require 'includes/footer.php'?>   
