aaca23da59043f12fa7bebb59dc8214b0bcf14a8
Shaine123 <shaineberdida@gmail.com>
2023-05-09 04:10:29 -0700

    conn

--- shainePhpOrig/db/conn.php
+++ shainePhpOrig/db/conn.php
@@ -1,16 +1,15 @@
 <?php
 	try {
 		$dbhost = 'localhost';
-		$dbname='gfg';
+		$dbname='employee';
 		$dbuser = 'root';
 		$dbpass = '';
-		$connect = new PDO(
+		$conn = new PDO(
 "mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
 	}
 catch (PDOException $e) {
 		echo "Error : " . $e->getMessage() . "<br/>";
 		die();
 	}
-	$count = $connect->exec("DELETE FROM employee WHERE emp_id = 123 ");
-	echo("Number of rows to be deleted from the employee table is: ". $count);
+   
 ?>
